65103a8bd08c09fada67f5f4bbc4c617d2862e53
rakafebriansy <kerenr445@gmail.com>
2024-07-26 23:04:55 +0700

    chore: config migrations

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
             $table->morphs('tokenable');
-            $table->string('name');
+            $table->string('name')->charset('utf8')->collation('utf8_unicode_ci');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
