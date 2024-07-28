919b10d12444312fc867cfab346bedfed8746b1f
rakafebriansy <kerenr445@gmail.com>
2024-07-28 07:24:01 +0700

    fix: only_full_groupby problem'

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -57,134 +57,168 @@ class NotesController extends Controller
         ],500)); 
     }
     public function getNotePreviews(Request $request): JsonResponse
-{
-    try {
-        $notesQuery = Note::select(
-            'notes.id',
-            'notes.title',
-            'notes.thumbnail_name',
-            'notes.created_at',
-            'users.first_name',
-            'users.last_name',
-            'study_programs.name as study_program',
-            'universities.name as university'
-        )
-        ->selectRaw('COUNT(orders.id) AS download_count')
-        ->join('users', 'users.id', '=', 'notes.user_id')
-        ->join('study_programs', 'study_programs.id', '=', 'users.study_program_id')
-        ->join('universities', 'universities.id', '=', 'users.university_id')
-        ->leftJoin('orders', 'notes.id', '=', 'orders.note_id')
-        ->groupBy(
-            'notes.id',
-            'notes.title',
-            'notes.thumbnail_name',
-            'notes.created_at',
-            'users.first_name',
-            'users.last_name',
-            'study_programs.name',
-            'universities.name'
-        );
-
-        if ($request->has('university_id')) {
-            $notesQuery->where('universities.id', $request->university_id);
-        }
-
-        if ($request->has('study_program_id')) {
-            $notesQuery->where('study_programs.id', $request->study_program_id);
-        }
-
-        if ($request->has('keyword')) {
-            $notesQuery->whereRaw("LOWER(notes.title) LIKE ?", ['%' . strtolower($request->keyword) . '%']);
-        }
-
-        $notes = $notesQuery->orderBy('notes.title')->get();
-        $notes_wrapped = NotePreviewResource::collection($notes);
-        $total_notes = $notes_wrapped->count();
-
-        return response()->json([
-            'data' => $notes_wrapped,
-            'total' => $total_notes
-        ], 200);
-    } catch (\Exception $e) {
-        return response()->json([
-            'errors' => [
-                'data' => $e->getMessage()
-            ]
-        ], 500);
-    }
-}
-    public function getUploadedNotePreviews(Request $request): JsonResponse
     {
         try {
-            $user_id = Auth::user()->id;
-            $notes = Note::select('notes.id','notes.title','notes.thumbnail_name','notes.created_at','users.first_name','users.last_name','study_programs.name as study_program','universities.name as university')
-            ->selectRaw('COUNT(orders.id) AS download_count')
-            ->join('users','users.id','notes.user_id')
-            ->join('study_programs','study_programs.id','users.study_program_id')
-            ->join('universities','universities.id','users.university_id')
-            ->leftJoin('orders','notes.id','orders.note_id')
-            ->where('notes.user_id',$user_id)
-            ->whereRaw("LOWER(notes.title) LIKE '%". strtolower($request->keyword)."%'")
-            ->groupBy(        
+            $notesQuery = Note::select(
                 'notes.id',
                 'notes.title',
                 'notes.thumbnail_name',
                 'notes.created_at',
-            )->orderBy('notes.title')->get();
+                'users.first_name',
+                'users.last_name',
+                'study_programs.name as study_program',
+                'universities.name as university'
+            )
+            ->selectRaw('COUNT(orders.id) AS download_count')
+            ->join('users', 'users.id', '=', 'notes.user_id')
+            ->join('study_programs', 'study_programs.id', '=', 'users.study_program_id')
+            ->join('universities', 'universities.id', '=', 'users.university_id')
+            ->leftJoin('orders', 'notes.id', '=', 'orders.note_id')
+            ->groupBy(
+                'notes.id',
+                'notes.title',
+                'notes.thumbnail_name',
+                'notes.created_at',
+                'users.first_name',
+                'users.last_name',
+                'study_programs.name',
+                'universities.name'
+            );
 
+            if ($request->has('university_id')) {
+                $notesQuery->where('universities.id', $request->university_id);
+            }
+
+            if ($request->has('study_program_id')) {
+                $notesQuery->where('study_programs.id', $request->study_program_id);
+            }
+
+            if ($request->has('keyword')) {
+                $notesQuery->whereRaw("LOWER(notes.title) LIKE ?", ['%' . strtolower($request->keyword) . '%']);
+            }
+
+            $notes = $notesQuery->orderBy('notes.title')->get();
             $notes_wrapped = NotePreviewResource::collection($notes);
             $total_notes = $notes_wrapped->count();
 
             return response()->json([
                 'data' => $notes_wrapped,
                 'total' => $total_notes
-            ])->setStatusCode(200);
-        } catch (\PDOException $e) {
-            throw new HttpResponseException(response([
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
                 'errors' => [
-                    'data' => [
-                        'Data is not found'
-                    ]
+                    'data' => $e->getMessage()
                 ]
-            ],500));
+            ], 500);
         }
     }
+    public function getUploadedNotePreviews(Request $request): JsonResponse
+    {
+        try {
+            $user_id = Auth::user()->id;
+            
+            $notesQuery = Note::select(
+                'notes.id',
+                'notes.title',
+                'notes.thumbnail_name',
+                'notes.created_at',
+                'users.first_name',
+                'users.last_name',
+                'study_programs.name as study_program',
+                'universities.name as university'
+            )
+            ->selectRaw('COUNT(orders.id) AS download_count')
+            ->join('users', 'users.id', '=', 'notes.user_id')
+            ->join('study_programs', 'study_programs.id', '=', 'users.study_program_id')
+            ->join('universities', 'universities.id', '=', 'users.university_id')
+            ->leftJoin('orders', 'notes.id', '=', 'orders.note_id')
+            ->where('notes.user_id', $user_id);
+
+            if ($request->has('keyword')) {
+                $notesQuery->whereRaw("LOWER(notes.title) LIKE ?", ['%' . strtolower($request->keyword) . '%']);
+            }
+
+            $notesQuery->groupBy(
+                'notes.id',
+                'notes.title',
+                'notes.thumbnail_name',
+                'notes.created_at',
+                'users.first_name',
+                'users.last_name',
+                'study_programs.name',
+                'universities.name'
+            )
+            ->orderBy('notes.title');
+
+            $notes = $notesQuery->get();
+            $notes_wrapped = NotePreviewResource::collection($notes);
+            $total_notes = $notes_wrapped->count();
+
+            return response()->json([
+                'data' => $notes_wrapped,
+                'total' => $total_notes
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'errors' => [
+                    'data' => 'An error occurred: ' . $e->getMessage()
+                ]
+            ], 500);
+        }
+    }
+
     public function getDownloadedNotePreviews(Request $request): JsonResponse
     {
         try {
             $user_id = Auth::user()->id;
-            $notes = Note::select('notes.id','notes.title','notes.thumbnail_name','notes.created_at','users.first_name','users.last_name','study_programs.name as study_program','universities.name as university')
+            
+            $notesQuery = Note::select(
+                'notes.id',
+                'notes.title',
+                'notes.thumbnail_name',
+                'notes.created_at',
+                'users.first_name',
+                'users.last_name',
+                'study_programs.name as study_program',
+                'universities.name as university'
+            )
             ->selectRaw('COUNT(orders.id) AS download_count')
-            ->join('users','users.id','notes.user_id')
-            ->join('study_programs','study_programs.id','users.study_program_id')
-            ->join('universities','universities.id','users.university_id')
-            ->join('orders','notes.id','orders.note_id')
-            ->where('orders.user_id',$user_id)
-            ->where('status','paid')
-            ->groupBy(        
-            'notes.id',
-            'notes.title',
-            'notes.thumbnail_name',
-            'notes.created_at',
-            )->orderBy('notes.title')->get();
-
+            ->join('users', 'users.id', '=', 'notes.user_id')
+            ->join('study_programs', 'study_programs.id', '=', 'users.study_program_id')
+            ->join('universities', 'universities.id', '=', 'users.university_id')
+            ->join('orders', 'notes.id', '=', 'orders.note_id')
+            ->where('orders.user_id', $user_id)
+            ->where('orders.status', 'paid')
+            ->groupBy(
+                'notes.id',
+                'notes.title',
+                'notes.thumbnail_name',
+                'notes.created_at',
+                'users.first_name',
+                'users.last_name',
+                'study_programs.name',
+                'universities.name'
+            )
+            ->orderBy('notes.title');
+            
+            $notes = $notesQuery->get();
             $notes_wrapped = NotePreviewResource::collection($notes);
             $total_notes = $notes_wrapped->count();
 
             return response()->json([
                 'data' => $notes_wrapped,
                 'total' => $total_notes
-            ])->setStatusCode(200);
-        } catch (\PDOException $e) {
-            throw new HttpResponseException(response([
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
                 'errors' => [
-                    'data' => [
-                        'Data is not found'
-                    ]
+                    'data' => 'An error occurred: ' . $e->getMessage()
                 ]
-            ],500));
+            ], 500);
         }
     }
+
     public function getSingleNote(Request $request): JsonResponse
     {
         try {
